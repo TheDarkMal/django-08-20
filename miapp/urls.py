6f75dda592ddf47e404a13ba82a1ffb28b5e5c92
mauricio vergara <mauricio301051020@gmail.com>
2019-08-21 18:01:46 -0500

    nmero1

--- miapp/urls.py
+++ miapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import cargar_inicio, LibroList
+from .views import cargar_inicio, LibroList, LibroCreate
 
 
 urlpatterns = [
     path('', cargar_inicio, name = 'inicio'),
     path('libros/', LibroList.as_view(), name='listar_libros'),
+    path('libros/nuevo/', LibroCreate.as_view(),name = 'nuevo_libro')
 ]
--- miapp/views.py
+++ miapp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 
+from django.urls import reverse_lazy
 from .models import Libro
 
 # Create your views here.
@@ -17,3 +18,10 @@ class LibroList(ListView):
 class LibroCreate():
     model = Libro
     template_name ='miapp/nuevo_libro.html'
+
+class LibroCreate(CreateView):
+    model = Libro
+    fields = ['nombre','descripcion','isbn','copias']
+    template_name = 'miapp/nuevo_libro.html'
+    success_url = reverse_lazy('listar_libros')
+
